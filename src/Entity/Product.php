f658a2acb0e28bb9a1082e6f738a14164c89be54
rodrigopepato <rodrigo.pepato@gmail.com>
2025-02-26 16:53:26 -0300

    Criação da entity 'Product'

---
+++ src/Entity/Product.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+class Product
+{
+    private ?int $id = null;
+
+    public function __construct(
+        private string $nome,
+        private string $descricao,
+        private string $preco
+    ) {}
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function getDescricao(): string
+    {
+        return $this->descricao;
+    }
+
+    public function getPreco(): string
+    {
+        return number_format((float) $this->preco, 2, '.', '');
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function setNome(string $nome): void
+    {
+        $this->nome = $nome;
+    }
+
+    public function setDescricao(string $descricao): void
+    {
+        $this->descricao = $descricao;
+    }
+
+    public function setPreco(string $preco): void
+    {
+        if (!is_numeric($preco)) {
+            throw new \InvalidArgumentException("O preço deve ser um número válido.");
+        }
+        $this->preco = number_format((float) $preco, 2, '.', '');
+    }
+}
